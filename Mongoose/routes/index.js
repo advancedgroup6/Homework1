0a30b248c00993b1db1d919f24c66e3db8092d22
swati4jha <swati4jha@gmail.com>
2017-09-26 21:16:55 -0400

    Update index.js

--- Mongoose/routes/index.js
+++ Mongoose/routes/index.js
@@ -60,13 +60,13 @@ router.post('/signup',function(req, resp, next){
         }else{
             // console.log(product);
             var user = product;
-            var token = jwt.sign(user,user.emailID, { expiresIn: 1440 });
+            var token = jwt.sign(user,user.emailID, { expiresIn: 14400 });
             resp.json({"status":"200","message":"sign-up success", "token":token,"userDetails":product});
         }
     });
 });
 
-router.post('/login', function(req, resp, next){
+    router.post('/login', function(req, resp, next){
     var emailID = req.body.emailID;// hard code the item to update the ID.
     var password = req.body.password;
     UserData.find({emailID:emailID}, function(err, doc){
@@ -76,8 +76,8 @@ router.post('/login', function(req, resp, next){
         console.log(doc);
         var user = doc[0];
 
-        if(password == user.password){
-            var token = jwt.sign(user,user.emailID, { expiresIn: 1440 });
+        if(user!=undefined && password == user.password){
+            var token = jwt.sign(user,user.emailID, { expiresIn: 14400 });
             resp.json({"status":"200", "message":"login success","token":token,"userDetails":user});
         }else{
             resp.json({"status":"500", "message":"couldn't login in."});
@@ -88,16 +88,16 @@ router.post('/login', function(req, resp, next){
     // UserData.findById(emailID, );
 });
 
-function checkToken(req) {
+function checkToken(token,emailID) {
 
-    const token = req.headers['x-access-token'];
+    //const token = req.headers['x-access-token'];
     console.log(token);
     if (token) {
-        console.log(req.body.emailID);
+        console.log(emailID);
         try {
-            var decoded = jwt.verify(token, req.body.emailID);
-            console.log("abc");
-            return decoded.message === req.params.id;
+            var decoded = jwt.verify(token, emailID);
+            console.log( decoded._doc.emailID);
+            return decoded._doc.emailID === emailID;
 
         } catch(err) {
             console.log("error");
@@ -114,63 +114,84 @@ router.post('/sendMsg',function(req, resp, next){
     console.log('came to insert');
 
     var majorminor = req.body.majorminor;
-    Region.find({majorminor:majorminor}, function(err, doc) {
-        if (err) {
-            console.error('Error finding proper beacon.');
-        } else {
-            var category = doc[0].category;
-            console.log(category);
-            var item = {
-                sender : req.body.sender,
-                receiver:req.body.receiver,
-                date:new Date(),
-                isRead:false,
-                isLocked:true,
-                msg:req.body.msg,
-                region:category,
-                majorminor:majorminor
-            };
-            // item needs to have the same structure as defined in the schema of the UserData
-            var data = new MsgData(item);
-            data.save(function(err, product, numAffected){
-                if(err){
-                    resp.json({"status":"500", "message":"internal server error"});
-                }else{
-                    // console.log(product);
-                    var msg = product;
-                    resp.json({"status":"200","message":"Message sent successfully!!", "mgsDetails":msg});
-                }
-            });
-        }
-    });
-});
-
-
-router.get('/deleteMsg/:id', function(req, resp, next){
-    var id = req.params.id;
-    MsgData.deleteOne({_id:id}, function(err, doc) {
-        if (err) {
-            console.error('Error deleting message.');
-        } else {
-            resp.json({"status":"200","message":"Message successfully deleted!"});
-        }
-    });
-});
-
-router.get('/updateMsgRead/:id', function(req, resp, next){
-    var id = req.params.id;
-    MsgData.updateOne(
-        {_id:id},
-        {
-            $set: { isRead: true }
-        }, function(err, messg){
+    var token = req.body.token;
+    var emailID = req.body.sender;
+    if(checkToken(token,emailID)){
+        Region.find({majorminor:majorminor}, function(err, doc) {
             if (err) {
                 console.error('Error finding proper beacon.');
-                resp.json({"status":"200","message":"Error updating message!!"});
             } else {
-                resp.json({"status":"200","message":"Message updated successfully!!"});
+                var category = doc[0].category;
+                console.log(category);
+                var item = {
+                    sender : req.body.sender,
+                    receiver:req.body.receiver,
+                    date:new Date(),
+                    isRead:false,
+                    isLocked:true,
+                    msg:req.body.msg,
+                    region:category,
+                    majorminor:majorminor
+                };
+                // item needs to have the same structure as defined in the schema of the UserData
+                var data = new MsgData(item);
+                data.save(function(err, product, numAffected){
+                    if(err){
+                        resp.json({"status":"500", "message":"internal server error"});
+                    }else{
+                        // console.log(product);
+                        var msg = product;
+                        resp.json({"status":"200","message":"Message sent successfully!!", "mgsDetails":msg});
+                    }
+                });
             }
         });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
+});
+
+
+router.get('/deleteMsg/:id/:token/:emailID', function(req, resp, next){
+    var id = req.params.id;
+    var token = req.params.token;
+    var emailID = req.params.emailID;
+    if(checkToken(token,emailID)){
+        MsgData.deleteOne({_id:id}, function(err, doc) {
+            if (err) {
+                console.error('Error deleting message.');
+            } else {
+                resp.json({"status":"200","message":"Message successfully deleted!"});
+            }
+        });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
+});
+
+    router.get('/updateMsgRead/:id/:token/:emailID', function(req, resp, next){
+    var id = req.params.id;
+    var token = req.params.token;
+    var emailID = req.params.emailID;
+    if(checkToken(token,emailID)){
+        MsgData.updateOne(
+            {_id:id},
+            {
+                $set: { isRead: true }
+            }, function(err, messg){
+                if (err) {
+                    console.error('Error finding proper beacon.');
+                    resp.json({"status":"200","message":"Error updating message!!"});
+                } else {
+                    resp.json({"status":"200","message":"Message updated successfully!!"});
+                }
+            });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
 });
 
 router.get('/updateMsgLock/:id', function(req, resp, next){
@@ -189,16 +210,59 @@ router.get('/updateMsgLock/:id', function(req, resp, next){
         });
 });
 
-router.get('/getMessages/:receiver', function(req, resp, next){
+router.get('/updateMsgs/:receiver/:region/:token', function(req, resp, next){
+    var receiver = req.params.receiver;
+    var region = req.params.region;
+    var token = req.params.token;
+    var emailID = receiver;
+    console.log("Inside update");
+    if(checkToken(token,emailID)){
+        console.log("Inside update1");
+        console.log(receiver);
+        console.log(region);
+        MsgData.updateMany(
+            {receiver:receiver,majorminor:region},
+            {
+                $set: { isLocked: false }
+            }, function(err, messg){
+                if (err) {
+                    console.error('Error finding proper beacon.');
+                    resp.json({"status":"200","message":"Error updating message!!"});
+                } else {
+                    MsgData.find({receiver:receiver}).sort({"date": -1}).exec(function(err, messg) {
+                        if (err) {
+                            console.error('Error updating message!!');
+                        } else {
+                            console.log(messg);
+                            resp.json({"status":"200","message":"Message fetched successfully!!", "msgList":messg});
+                        }
+                    });
+
+                }
+            });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
+});
+
+router.get('/getMessages/:receiver/:token', function(req, resp, next){
     var receiverID = req.params.receiver;
-    MsgData.find({receiver:receiverID}).sort({"date": -1}).exec(function(err, messg) {
-        if (err) {
-            console.error('Error finding proper beacon.');
-        } else {
-            console.log(messg);
-            resp.json({"status":"200","message":"Message fetched successfully!!", "msgList":messg});
-        }
-    });
+    var token = req.params.token;
+    var emailID = receiverID;
+    if(checkToken(token,emailID)){
+        MsgData.find({receiver:receiverID}).sort({"date": -1}).exec(function(err, messg) {
+            if (err) {
+                console.error('Error finding proper beacon.');
+            } else {
+                console.log(messg);
+                resp.json({"status":"200","message":"Message fetched successfully!!", "msgList":messg});
+            }
+        });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
 });
 
 router.get('/getAllMsgs', function(req, resp, next){
@@ -213,30 +277,44 @@ router.get('/getAllMsgs', function(req, resp, next){
 });
 
 
-router.get('/getAllUsers', function(req, resp, next){
-    UserData.find({} ,function(err, messg) {
-        if (err) {
-            console.error('Error finding proper beacon.');
-        } else {
-            console.log(messg);
-            for(i=0;i<messg.length;i++){
-                messg[i].password="";
+router.get('/getAllUsers/:token/:emailID', function(req, resp, next){
+    var token = req.params.token;
+    var emailID = req.params.emailID;
+    if(checkToken(token,emailID)){
+        UserData.find({} ,function(err, messg) {
+            if (err) {
+                console.error('Error finding proper beacon.');
+            } else {
+                console.log(messg);
+                for(i=0;i<messg.length;i++){
+                    messg[i].password="";
+                }
+                resp.json({"status":"200","message":"Users fetched successfully!!", "usersList":messg});
             }
-            resp.json({"status":"200","message":"Users fetched successfully!!", "usersList":messg});
-        }
-    });
+        });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
 });
 
 
-router.get('/getAllRegions', function(req, resp, next){
-    Region.find({} ,function(err, messg) {
-        if (err) {
-            console.error('Error finding proper beacon.');
-        } else {
-            console.log(messg);
-            resp.json({"status":"200","message":"Message sent successfully!!", "regionList":messg});
-        }
-    });
+router.get('/getAllRegions/:token/:emailID', function(req, resp, next){
+    var token = req.params.token;
+    var emailID = req.params.emailID;
+    if(checkToken(token,emailID)){
+        Region.find({} ,function(err, messg) {
+            if (err) {
+                console.error('Error finding proper beacon.');
+            } else {
+                console.log(messg);
+                resp.json({"status":"200","message":"Message sent successfully!!", "regionList":messg});
+            }
+        });
+    } else{
+        resp.json({"status":"500", "message":"Login error"});
+    }
+
 });
 
 
